fc494dc595fd91b4ce2f4e42735e9f61be28f4a5
Anatoli Kamyshev <36002768+Scwarz@users.noreply.github.com>
2025-01-23 20:47:14 +0000

    another code editing for mix_some_reagents

--- boiler.rb
+++ boiler.rb
@@ -19,7 +19,9 @@ class Boiler
       puts '1) List of Reagents'
       puts '2) Mix some Reagents'
       puts '3) Exit'
-      
+
+      user_choice = gets.chomp.to_i
+
       case user_choice
       when 1
         list_of_reagents
@@ -28,14 +30,17 @@ class Boiler
       # Exit the loop if a user pushes "3) exit"
       when 3
         puts 'Stop preparing the potion. Bye-bye!'
-      
-    end
-  end  
+        break # Exit the loop
+      else
+        puts 'Invalid option. Please choose again.'
+      end # End of case block
+    end # End of loop
+  end
 
   def list_of_reagents
     puts 'What we have in our pockets that we could use for making the potion:'
     @reagents = ['Human nail', 'Cat claw', 'Mouse tail']
-
+    @reagents.each { |reagent| puts reagent }
   end
 
   def mix_some_reagents
@@ -43,31 +48,30 @@ class Boiler
       puts 'Please choose some of the reagents first!'
       return
     end
+
     puts 'Choose two reagents by typing their names:'
     print 'Type the first reagent: '
     reagent1 = gets.downcase.chomp
     print 'Type the second reagent: '
     reagent2 = gets.downcase.chomp
-    if @reagents.include?(reagent1) && @reagents.include?(reagent2)
-      reagent = Reagent.new
-      mixed_reagents = reagent.add(reagent1.capitalize, reagent2.capitalize)
-      puts "You mixed: #{mixed_reagents}"
-    else
-      # Special messages for specific combinations
-      special_messages = {
-      ['human nail', 'cat claw'] => "Oh no, you turned a human into a cat! Run, Pinky!",
-      ['cat claw', 'mouse tail'] => "Not bad, Pinky! You turned all cats into mice. At least we are safe from cats, but we still need to take over the world! Keep trying!",
-      ['human nail', 'mouse tail'] => "You've created a tiny human-mouse hybrid. Fascinating! Now, it's time to conquer the world!"
-    }
-    key = [reagent1, reagent2].sort # Have to google it. Stuck with the sorting because what if a user types "cat claw and human nail" so that will be a different combination, but has to have the same message. So I sorted it to make it to ensure order doesn't matter
-    if special_messages.key?(key)
-      puts special_messages[key]
-    else
-      puts "You mixed: #{mixed_reagents}. No strange effects... yet. Try again"
-    end
-      puts 'Invalid reagents. Please select from the list of reagents.'
-    end
 
+    # Special messages for specific combinations
+    special_messages = {
+      ['human nail', 'cat claw'] => "Oh no, you turned a human into a cat! Run, Pinky!",
+      ['cat claw', 'human nail'] => "Oh no, you turned a human into a cat! Run, Pinky!",
+      ['cat claw', 'mouse tail'] => "Not bad, Pinky! You turned all cats into mice. At least we are safe from cats, but we still need to take over the world! Keep trying!",
+      ['mouse tail', 'cat claw'] => "Not bad, Pinky! You turned all cats into mice. At least we are safe from cats, but we still need to take over the world! Keep trying!",
+      ['human nail', 'mouse tail'] => "You've created a tiny human-mouse hybrid. Fascinating! Now, it's time to conquer the world!",
+      ['mouse tail', 'human nail'] => "You've created a tiny human-mouse hybrid. Fascinating! Now, it's time to conquer the world!"
+    }
+    #Have to google it since my knolodges are not strong. So I was stuck with a vlidation that both entered reagents are valid and are on the list. So I converted reagents to downcase and check if both are valid
+    reagents = [reagent1, reagent2].map(&:downcase)
+  if (@reagents.map(&:downcase) & reagents).size == 2
+    key = reagents.sort
+    puts special_messages.fetch(key, "You mixed: #{reagent1.capitalize} and #{reagent2.capitalize}. No strange effects... yet. Try again")
+  else
+    puts 'Invalid reagents. Please select from the list of reagents.'
+  end
   end
 
   def print_greetings
@@ -77,3 +81,4 @@ class Boiler
   def print_bye
     puts "Thank you for helping the Brain in mixing reagents for the potion. See you!"
   end
+end
