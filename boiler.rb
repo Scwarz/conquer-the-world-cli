23b8e45d61397e6cd66ad4da48502d9b96c3afff
Anatoli Kamyshev <36002768+Scwarz@users.noreply.github.com>
2025-01-23 19:59:40 +0000

    added a code mix_some_reagents

--- boiler.rb
+++ boiler.rb
@@ -19,13 +19,33 @@ class Boiler
       puts '1) List of Reagents'
       puts '2) Mix some Reagents'
       puts '3) Exit'
-
-      # Exit the loop if a user pushes "3) exit"
       
-
+      case user_choice
+      when 1
+        list_of_reagents
+      when 2
+        mix_some_reagents
+      # Exit the loop if a user pushes "3) exit"
+      when 3
+        puts 'Stop preparing the potion. Bye-bye!'
       
     end
   end  
+
+  def list_of_reagents
+    puts 'What we have in our pockets that we could use for making the potion:'
+    @reagents = ['Human nail', 'Cat claw', 'Mouse tail']
+
+  end
+
+  def mix_some_reagents
+    if @reagents.empty?
+      puts 'Please choose some of the reagents first!'
+      return
+    end
+    puts 'Choose two reagents by typing their names:'
+    
+  end
 
   def print_greetings
     puts "\nHey, Pinky! I need your help!"
